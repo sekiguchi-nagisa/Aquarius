382d3ac5a0cecc9646ae7674c87a1f9dd4472193
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2014-10-06 02:37:40 +0900

    add bootstrap grammar

--- src/aquarius/runtime/ParsingActionSet.java
+++ src/aquarius/runtime/ParsingActionSet.java
@@ -6,6 +6,25 @@ package aquarius.runtime;
  *
  */
 public interface ParsingActionSet {
-	public ParsedResult action();
-	public boolean predicate();
+	/**
+	 * 
+	 * @param argList
+	 * the results of preceding expression.
+	 * @param index
+	 * none negative number.
+	 * @return
+	 * result of action
+	 */
+	public ParsedResult action(ResultList argList, int index);
+
+	/**
+	 * 
+	 * @param argList
+	 * the results of preceding expression.
+	 * @param index
+	 * none negative number
+	 * @return
+	 * result of action
+	 */
+	public boolean predicate(ResultList argList, int index);
 }
